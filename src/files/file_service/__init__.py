0e6cd27649ddada14d2f5e13c39a985fffc702d4
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-11-13 09:04:21 -0500

    chore: removed old filesAPI

--- src/files/file_service/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing File Service"""
-
-from .schema_api_httpx_client import FilePayload, SchemaApiHttpxClient
-
-__all__ = (
-    "SchemaApiHttpxClient",
-    "FilePayload",
-)
