09ee80bf638a951b4648e45e62dae00cd97f945c
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-12-17 13:25:31 -0500

    Abfe run fixes (#398)

--- tests/run_abfe.py
+++ tests/run_abfe.py
@@ -11,29 +11,42 @@ def run_abfe():
     print("Preparing system...")
 
     _ = sim.prepare(ligand=ligand, padding=1.0)
-    print("System prepared")
+    print("✅ System prepared", flush=True)
 
     sim.abfe.set_test_run(1)
 
-    output_dir_path = f"/tests/{uuid.uuid4()}"
+    # set up output directory on ufa
+    output_dir_path = f"/tests/{uuid.uuid4()}/"
+
+    # get a quote for the job
     jobs = sim.abfe.run(ligands=[ligand], quote=True, output_dir_path=output_dir_path)
     job = jobs[0]
 
     assert job.status == "Quoted", (
-        f"Expected job to be quoted, instead got {job.status}"
+        f"❌ Expected job to be quoted, instead got {job.status}"
     )
 
-    print("Job quoted")
+    print("✅ Job quoted", flush=True)
 
-    print(job.id)
+    print(job.id, flush=True)
 
     job.confirm()
 
-    time.sleep(20)
+    timeout_seconds = 20
+    start_time = time.time()
+    while job.status != "Running":
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= timeout_seconds:
+            raise TimeoutError(
+                f"❌ Job did not start running within {timeout_seconds} seconds. "
+                f"❌ Current status: {job.status}"
+            )
+        time.sleep(2)
+        job.sync()
 
     # check that job is running
     assert job.status == "Running", (
-        f"Expected job to be running, instead got {job.status}"
+        f"❌Expected job to be running, instead got {job.status}"
     )
 
 
