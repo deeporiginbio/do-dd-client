a09bf320ca1e9a7fc56d5a2783b75fc4c58ebe3a
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-12-17 12:19:50 -0500

    Protonation fixes (#396)

--- tests/test_ligand.py
+++ tests/test_ligand.py
@@ -19,6 +19,23 @@ from tests.utils_ligands import (
 base_path = os.path.join(os.path.dirname(__file__), "fixtures")
 
 
+def test_ligand_add_hydrogens():
+    ligand = Ligand.from_smiles("Oc1cccc(-c2nnc3ccc(-c4ccoc4)cn23)c1")
+    initial_smiles = ligand.smiles
+    ligand.add_hydrogens()
+    assert ligand.smiles != initial_smiles, (
+        "Expected the smiles to change after adding hydrogens"
+    )
+    assert "H" in ligand.smiles, "Expected the smiles to contain hydrogens"
+
+
+def test_ligand_has_hydrogens():
+    ligand = Ligand.from_smiles("Oc1cccc(-c2nnc3ccc(-c4ccoc4)cn23)c1")
+    assert not ligand.has_hydrogens(), "Expected this ligand to not have hydrogens"
+    ligand.add_hydrogens()
+    assert ligand.has_hydrogens(), "Expected this ligand to have hydrogens"
+
+
 @pytest.mark.parametrize("ligand_file", single_ligand_files)
 def test_ligand_hash_stable(ligand_file):
     """check that the ligand hash doesn't change if we perform various read-only operations"""
