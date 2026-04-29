f528598b4bf66fb91d69d50e81b28f4d0bf3fd93
Srinivas Gorur-Shandilya <code@srinivas.gs>
2026-04-29 10:43:13 -0400

    fix: bumped tool for docking (#534)

--- src/platform/constants.py
+++ src/platform/constants.py
@@ -52,7 +52,7 @@ PROVIDER = Literal["ufa", "s3"]
 TOOL_KEYS_AND_VERSIONS: dict[str, dict[str, str]] = {
     "docking": {
         "tool_key": "deeporigin.docking",
-        "tool_version": "3.1.7",
+        "tool_version": "3.1.11",
     },
     "pocket_finder": {
         "function_key": "deeporigin.pocketfinder",
